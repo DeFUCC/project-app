e27099879f8f4822d689412a8f038d77cb7890de
DeFUCC <fond@fondfrukt.ru>
2021-01-18 23:37:08 +0300

    moved items fetch back to the list component

--- src/components/items/list.vue
+++ src/components/items/list.vue
@@ -3,23 +3,26 @@
   items-header(
     :parent="parent",
     :type="type",
-    :options="items.options",
-    :sorted="items.sorted"
+    :options="options",
+    :sorted="sorted"
   )
   add-form(:type="type", :parent="parent", v-if="editable")
   ul.item-list
     transition-group(name="list")
       item-card.card(
         @click="$emit('open', item.soul)",
-        v-for="item in items.sorted.list",
+        v-for="item in sorted.list",
         :key="item.soul",
         :item="item"
       )
 </template>
 
-<script>
+<script lang="ts">
 import { computed, defineComponent, reactive, ref, watchEffect } from "vue";
+import { appPath, db, gun, soul } from "../../store/gun-db";
+import { user } from "../../store/user";
 import { useItems } from "../../use/items";
+import { useSorter } from "../../use/sorter";
 
 export default defineComponent({
   name: "ItemsList",
@@ -34,22 +37,71 @@ export default defineComponent({
   },
 
   setup(props) {
-    const items = useItems({
-      type: props.type,
-      parent: props.parent,
-      user: props.user,
-    });
+    const items = reactive({});
 
-    const ver = ref(1);
+    let query: any;
+    if (props.parent) {
+      query = gun.get(props.parent);
+    } else {
+      query = db;
+    }
+    query.get(props.type).map().on(loadItem);
 
-    function update() {
-      ver.value++;
+    const { sorted, options } = useSorter(items);
+
+    async function loadItem(data: any, key: string) {
+      if (!data) {
+        return;
+      }
+      if (items[key]) {
+        items[key] = null;
+      }
+      items[key] = { ...data };
+      let item = items[key];
+      item.soul = soul(data);
+
+      item.rated = {
+        star: {},
+        seen: {},
+        trash: {},
+      };
+
+      item.myRate = {
+        star: false,
+        seen: false,
+        trash: false,
+      };
+
+      db.get("user")
+        .map()
+        .on((userData, userId) => {
+          for (let rate in item.rated) {
+            gun
+              .user(userId)
+              .get(appPath)
+              .get("rate")
+              .get(rate)
+              .get(soul(data))
+              .on((rated: any, rateType: string) => {
+                if (rated) {
+                  item.rated[rate][userId] = true;
+                  if (userId == user?.is?.pub) {
+                    item.myRate[rate] = true;
+                  }
+                } else {
+                  delete item.rated[rate][userId];
+                  if (userId == user?.is?.pub) {
+                    item.myRate[rate] = false;
+                  }
+                }
+              });
+          }
+        });
     }
 
     return {
-      items,
-      ver,
-      update,
+      sorted,
+      options,
     };
   },
 });
